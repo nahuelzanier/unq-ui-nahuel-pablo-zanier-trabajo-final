08d221cd5f506ca6a5e24bb52f2f3eb4ea7c520b
nahuelpabloz <nahuelpabloz@gmail.com>
2024-07-01 22:31:46 -0300

    api

---
+++ src/api/PreguntadosApi.js
@@ -0,0 +1,34 @@
+const url = "https://preguntados-api.vercel.app"
+
+const getDifficulty = async (setDifficulty) => {
+    let response = await fetch(url + `/api/difficulty`)
+    let data = await response.json()
+    setDifficulty(data)
+}
+
+const getQuestions = async (difficulty, setQuestions) => {
+    let response = await fetch(url + `/api/questions?difficulty=${difficulty}`)
+    let data = await response.json()
+    setQuestions(data)
+}
+
+const postAnswer = async (questionId, option, setAnswer) => {
+    let response = await fetch(url + `/api/answer`, {
+        method: "POST",
+        body: JSON.stringify({
+            questionId: questionId,
+            option: option
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    let data = await response.json()
+    setAnswer(data)
+}
+
+export const Api = {
+    getDifficulty,
+    getQuestions, 
+    postAnswer
+}
